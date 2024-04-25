8718b7e06b6f8416183db05baf1ca1c59eac8ea2
Bogdan VASILIU <bogdan.vasiliu@morningstar.com>
2024-04-25 21:19:43 +0300

    added nutrition api

--- ConsoleApi/Program.cs
+++ ConsoleApi/Program.cs
@@ -1,10 +1,24 @@
 ﻿using ClassLibrary1;
+using MyAPICode;
 
-var api = new DogApi();
-for (int i = 0; i < 10; i++)
-{
-    var result = await api.GetRandomDogAsync("african");
-    Console.WriteLine(result.Status);
-    Console.WriteLine(result.Message);
-}
+//await UseDogApi();
+UseNutritionApi();
 Console.ReadKey();
+
+static void UseNutritionApi()
+{
+    var api = new NutritionAPI();
+    var result = api.GetNutritionData();
+    Console.WriteLine(result);
+}
+
+static async Task UseDogApi()
+{
+    var api = new DogApi();
+    for (int i = 0; i < 10; i++)
+    {
+        var result = await api.GetRandomDogAsync("african");
+        Console.WriteLine(result.Status);
+        Console.WriteLine(result.Message);
+    }
+}
---
+++ MyAPICode/NutritionAPI.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace MyAPICode
+{
+    public class NutritionAPI
+    {
+        private string _apiURL = "https://api.edamam.com/api/nutrition-data";
+        public NutritionAPI()
+        {
+
+        }
+
+        public string? GetNutritionData()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("ApiKey");
+            var secretKey = Environment.GetEnvironmentVariable("SecretKey");
+            var options = new RestClientOptions(_apiURL)
+            {
+                Authenticator = new HttpBasicAuthenticator(apiKey, secretKey)
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("?ingr=milk");
+            
+            var response = client.GetAsync(request).Result;
+            return response.Content;
+        }
+    }
+}
