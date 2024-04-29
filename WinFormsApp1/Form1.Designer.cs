b6a442068dd9fa6d305ef99cb58369445c41b749
Bogdan VASILIU <bogdan.vasiliu@morningstar.com>
2024-04-29 17:06:00 +0300

    add separate button for winforms async api call

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.pbDogImage = new System.Windows.Forms.PictureBox();
             this.btnFetchDog = new System.Windows.Forms.Button();
+            this.btnFetchDogAsync = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbDogImage)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,17 +46,28 @@
             // 
             this.btnFetchDog.Location = new System.Drawing.Point(826, 133);
             this.btnFetchDog.Name = "btnFetchDog";
-            this.btnFetchDog.Size = new System.Drawing.Size(150, 46);
+            this.btnFetchDog.Size = new System.Drawing.Size(299, 46);
             this.btnFetchDog.TabIndex = 1;
-            this.btnFetchDog.Text = "button1";
+            this.btnFetchDog.Text = "Fetch Dog";
             this.btnFetchDog.UseVisualStyleBackColor = true;
             this.btnFetchDog.Click += new System.EventHandler(this.btnFetchDog_Click);
+            // 
+            // btnFetchDogAsync
+            // 
+            this.btnFetchDogAsync.Location = new System.Drawing.Point(826, 228);
+            this.btnFetchDogAsync.Name = "btnFetchDogAsync";
+            this.btnFetchDogAsync.Size = new System.Drawing.Size(299, 46);
+            this.btnFetchDogAsync.TabIndex = 1;
+            this.btnFetchDogAsync.Text = "Fetch Dog Async";
+            this.btnFetchDogAsync.UseVisualStyleBackColor = true;
+            this.btnFetchDogAsync.Click += new System.EventHandler(this.btnFetchDogAsync_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1353, 944);
+            this.Controls.Add(this.btnFetchDogAsync);
             this.Controls.Add(this.btnFetchDog);
             this.Controls.Add(this.pbDogImage);
             this.Name = "Form1";
@@ -69,5 +81,6 @@
 
         private PictureBox pbDogImage;
         private Button btnFetchDog;
+        private Button btnFetchDogAsync;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -4,16 +4,25 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private readonly DogApi _dogApi;
         public Form1()
         {
             InitializeComponent();
+            _dogApi = new DogApi();
         }
 
         private void btnFetchDog_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
-            DogApi api = new DogApi();
-            DogApiResponse result = api.GetRandomDog("affenpinscher");
+            DogApiResponse result = _dogApi.GetRandomDog("affenpinscher");
+            if (result != null)
+            {
+                pbDogImage.ImageLocation = result.Message;
+            }
+        }
+
+        private async void btnFetchDogAsync_Click(object sender, EventArgs e)
+        {
+            DogApiResponse result = await _dogApi.GetRandomDogAsync("affenpinscher");
             if (result != null)
             {
                 pbDogImage.ImageLocation = result.Message;
