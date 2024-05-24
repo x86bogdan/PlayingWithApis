a57b897fb10b2636dd037de8c27f1c738e0d3cc2
Bogdan VASILIU <bogdan.vasiliu@morningstar.com>
2024-05-24 14:48:05 +0300

    crearea unui API pentru expunerea informatiilor procesate

---
+++ Api1/Controllers/HumanAndDogController.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api1.Controllers
+{
+    [ApiController]
+    [Route("human")]
+    public class HumanAndDogController : Controller
+    {
+        [HttpGet]
+        //[ProducesResponseType(IEnumerable<StudentAndDog>)]
+        public IActionResult Index()
+        {
+            var api = new HumanAndDog();
+            var result = api.GetStudentAndDog();
+            return Ok(result);
+        }
+    }
+}
