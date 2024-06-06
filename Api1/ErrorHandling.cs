1faf60110252bf47d3b1f52d1a518c660633e042
Bogdan VASILIU <bogdan.vasiliu@morningstar.com>
2024-06-06 22:17:29 +0300

    adaugarea unui middleware global pentru error handling

---
+++ Api1/ErrorHandling.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Api1
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                var result = JsonSerializer.Serialize(new { message = error?.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
